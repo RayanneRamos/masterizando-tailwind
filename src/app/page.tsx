332a4000f236223f4bab09582b38d3f621df038f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-04 15:53:20 -0300

    aprendendo o dark theme no tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hello World</h1>
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">
+          Hello World
+        </h1>
+      </div>
     </div>
   );
 }
