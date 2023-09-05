e368176452d0c56bde34d9b15d408c284622eb8e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-05 14:39:16 -0300

    criando o segundo menu da aplicação

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,11 @@
-import { Search } from 'lucide-react'
-import { Logo } from './Logo'
-import { MainNavigator } from './MainNavigation'
+import { Cog, LifeBuoy, Search } from "lucide-react";
+import { Logo } from "./Logo";
+import { MainNavigator } from "./MainNavigation";
+import { NavItem } from "./MainNavigation/NavItem";
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
       <div className=" mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
@@ -14,6 +15,12 @@ export function Sidebar() {
         />
       </div>
       <MainNavigator />
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+      </div>
     </aside>
-  )
+  );
 }
