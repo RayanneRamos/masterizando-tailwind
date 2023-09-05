dbf3997a36a54d4bd6a6843365b5acc2f7c0c8ee
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-05 15:02:18 -0300

    criando o grid do profile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,25 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable prettier/prettier */
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./src/**/*.tsx'],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       gridTemplateColumns: {
-        app: 'minmax(18rem, 20rem) 1fr',
+        app: "minmax(18rem, 20rem) 1fr",
+        profile: "max-content 1fr min-content",
       },
 
       maxWidth: {
-        app: '700px',
+        app: "700px",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
