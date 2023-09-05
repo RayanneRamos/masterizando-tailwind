4dc2c9627eab198ace3a817a0fac895b7e6e2f30
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-05 15:02:35 -0300

    criando o componente Profile

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { LogOut } from "lucide-react";
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        className="h-10 w-10 rounded-full"
+        src="https://github.com/RayanneRamos.png"
+        alt="Foto do usuário"
+      />
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Rayanne Ramos
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          rayanneramos@gmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  );
+}
