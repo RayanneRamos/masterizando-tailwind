a7dd66140271139fe23feee373591167cb34f412
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-05 15:55:22 -0300

    estrutura do formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
 /* eslint-disable prettier/prettier */
 import { SettingsTabs } from "@/components/SettingsTabs";
-import * as Tabs from "@radix-ui/react-tabs";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
       <SettingsTabs />
+      <div className="mt-6 flex flex-col ">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              className="rounded-lg border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+              type="button"
+            >
+              Cancel
+            </button>
+            <button
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              type="submit"
+              form="settings"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <form id="settings" className="mt-6 flex w-full flex-col "></form>
+      </div>
     </>
   );
 }
