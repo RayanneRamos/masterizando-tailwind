f0c95c3032b7ac8f9d986c5744e6b8ca71cfde86
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-05 15:14:43 -0300

    pattern de composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Cog, LifeBuoy, Search } from "lucide-react";
 import { Logo } from "./Logo";
 import { MainNavigator } from "./MainNavigation";
-import { NavItem } from "./MainNavigation/NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import { InputControl, InputPrefix, InputRoot } from "../Input";
+import { Search } from "lucide-react";
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <div className=" mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
       <MainNavigator />
       <UsedSpaceWidget />
       <div className="h-px bg-zinc-200" />
