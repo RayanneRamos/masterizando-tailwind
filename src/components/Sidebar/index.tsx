c5694015446c0acb308a220b072462976fa2187c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-18 15:39:16 -0300

    últimos ajustes no tema dark

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -33,7 +33,7 @@ export function Sidebar() {
         </InputRoot>
         <MainNavigator />
         <UsedSpaceWidget />
-        <div className="h-px bg-zinc-200" />
+        <div className="h-px bg-zinc-200 dark:bg-zinc-700" />
         <Profile />
       </Collapsible.Content>
     </Collapsible.Root>
